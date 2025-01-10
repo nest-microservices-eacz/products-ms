27dba47967ebe78491593d91f035ec2700f3daf6
Esteban Canteros <estebanacanteros@gmail.com>
2025-01-10 16:02:20 -0300

    transform restful endpoint in to a tcp microservice

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,25 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { envs } from './config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
   const logger = new Logger('MAIN');
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: { port: envs.port },
+    },
+  );
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
     }),
   );
-  await app.listen(envs.port);
-  logger.log(`App running on port ${envs.port}`);
+  logger.log(`Products microservices running on port ${envs.port}`);
+
 }
 bootstrap();
