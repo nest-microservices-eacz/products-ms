6e72c80ec38684d38074046a597fab1c53385e33
Esteban Canteros <estebanacanteros@gmail.com>
2025-01-13 15:02:21 -0300

    replace http methods decorators for @MessagePattern

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -58,8 +58,9 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
   }
 
   async update(id: number, updateProductDto: UpdateProductDto) {
+    const {id: _, ...data} = updateProductDto
     await this.findOne(id);
-    return this.product.update({ where: { id }, data: updateProductDto });
+    return this.product.update({ where: { id }, data});
   }
 
   async remove(id: number) {
