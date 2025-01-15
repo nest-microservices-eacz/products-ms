ba260e16c333907db0f2aac8d6e6c0311787d86b
Esteban Canteros <estebanacanteros@gmail.com>
2025-01-15 18:23:27 -0300

    fix app not listening for request

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,7 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+  await app.listen()
   logger.log(`Products microservices running on port ${envs.port}`);
 }
 bootstrap();
