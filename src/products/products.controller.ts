f2910f3b0af6b8c0248b7671c806d70abed1fd9f
Esteban Canteros <estebanacanteros@gmail.com>
2025-01-21 16:58:28 -0300

    add service to validate an array of ids

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -41,4 +41,9 @@ export class ProductsController {
   remove(@Payload('id', ParseIntPipe) id: number) {
     return this.productsService.remove(id);
   }
+
+  @MessagePattern({ cmd: 'validate_products' })
+  validateProducts(@Payload() ids: number[]) {
+    return this.productsService.validateProducts(ids)
+  }
 }
